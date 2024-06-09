67a9be7d1553eed8edf2f360166ba17692fd1ce2
Dotnet9(dotnet9.com) <1012434131@qq.com>
2024-06-09 15:54:16 +0800

    add NuGet test;

---
+++ src/WPFDemoUseNuget/Message/SayHelloMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace WPFDemoUseNuget.Message
+{
+    public class SayHelloMessage : CodeWF.EventBus.Message
+    {
+        public string Word { get; }
+
+        public SayHelloMessage(object sender, string word) : base(sender)
+        {
+            Word = word;
+        }
+    }
+}
