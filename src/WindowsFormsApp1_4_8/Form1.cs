7f388c2d02d8633a183800c22d91e3993e51d89b
Henry <1012434131@qq.com>
2025-04-29 14:16:04 +0800

    add winform demo;

---
+++ src/WindowsFormsApp1_4_8/Form1.cs
@@ -0,0 +1,31 @@
+﻿using CodeWF.EventBus;
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1_4_8
+{
+    public class TimeCommand : Command
+    {
+        public int Id { get; set; }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            EventBus.Default.Subscribe(this);
+
+            var btn = new Button();
+            btn.Text = "publish";
+            btn.Click += (s, e) => EventBus.Default.Publish(new TimeCommand() { Id = DateTime.Now.Millisecond });
+            this.Controls.Add(btn);
+        }
+
+        [EventHandler]
+        private void ReceiveCommand(TimeCommand command)
+        {
+            MessageBox.Show($"收到ID：{command.Id}");
+        }
+    }
+}
---
+++ src/WindowsFormsApp1_4_8/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的一般信息由以下
+// 控制。更改这些特性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("WindowsFormsApp1_4_8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApp1_4_8")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
+//请将此类型的 ComVisible 特性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("f683a892-b28d-46ee-b86b-2be2f4f4b997")]
+
+// 程序集的版本信息由下列四个值组成: 
+//
+//      主版本
+//      次版本
+//      生成号
+//      修订号
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
